c068a5fb4af58f8104fe575ac4a2be38f568ccab
apekshamishra03 <apekshamishra89@gmail.com>
2026-04-07 20:08:14 +0530

    final deploy fix

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from functools import wraps
 
 app = Flask(__name__)
 app.secret_key = "techprepsecret"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///techprep.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -102,7 +102,8 @@ def logout():
     return redirect(url_for('index'))
 
 
+import os
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
